298e39e5b2e5764f8a62c907d396db777eb58f40
javierolivero <jolivero707@gmail.com>
2018-12-08 09:53:27 -0600

    how chages Sat

---
+++ pyBank/main.py
@@ -0,0 +1,33 @@
+# This is HW3 - Budget Case
+
+# First I have to read the data
+
+import os
+import csv
+
+csvpath = os.path.join('..', 'HW3', 'budget_data.csv')
+
+# # Method 1: Plain Reading of CSV files
+# with open(csvpath, 'r') as file_handler:
+#     lines = file_handler.read()
+#     print(lines)
+#     print(type(lines))
+
+
+with open(csvpath, newline='') as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
+        
+
+
